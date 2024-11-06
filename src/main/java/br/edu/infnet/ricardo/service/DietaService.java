65f17ddd65c7ba9f060da1cbb583cca1d8ca9ed1
Ricardo da Silva Lima <rslima@gmail.com>
2024-11-06 09:12:55 -0300

    refactoring

--- src/main/java/br/edu/infnet/ricardo/service/DietaService.java
+++ src/main/java/br/edu/infnet/ricardo/service/DietaService.java
@@ -2,8 +2,6 @@ package br.edu.infnet.ricardo.service;
 
 import br.edu.infnet.ricardo.domain.Dieta;
 import br.edu.infnet.ricardo.repository.DietaRepository;
-import br.edu.infnet.ricardo.repository.RefeicaoRepository;
-import br.edu.infnet.ricardo.repository.UsuarioRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,10 +15,8 @@ import java.util.Optional;
 @RequiredArgsConstructor
 @Slf4j
 public class DietaService {
-    private final UsuarioRepository usuarioRepository;
     private final DietaRepository dietaRepository;
-    private final RefeicaoRepository refeicaoRepository;
-
+    
     public void salvar(Dieta dieta) {
         dietaRepository.save(dieta);
     }
@@ -32,7 +28,5 @@ public class DietaService {
     public Optional<Dieta> buscaPorIdUsuario(Long usuarioId) {
         return dietaRepository.findByUsuarioId(usuarioId);
     }
-
-
 
 }
