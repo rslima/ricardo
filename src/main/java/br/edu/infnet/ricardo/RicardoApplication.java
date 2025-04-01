36cf15f19d2e867f3c009c65a73e7ce03c8f6444
Ricardo da Silva Lima <rslima@senado.leg.br>
2025-04-01 08:40:16 -0300

    Adiciona starter de validação

--- src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
+++ src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RicardoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RicardoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RicardoApplication.class, args);
+    }
 
 }
