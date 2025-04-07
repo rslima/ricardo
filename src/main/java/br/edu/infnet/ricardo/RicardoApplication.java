0ef5713f20eef286e0dc21bc25a169e7a467595f
Ricardo da Silva Lima <rslima@gmail.com>
2025-04-07 16:16:25 -0300

    Prometheus Basic Setup

--- src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
+++ src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
@@ -1,13 +1,21 @@
 package br.edu.infnet.ricardo;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RicardoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RicardoApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config().commonTags("application", "Ricardo");
     }
 
 }
