683e286ed34c5ea939a5b6fca5650e54924b0938
Ricardo da Silva Lima <rslima@gmail.com>
2025-04-07 21:43:34 -0300

    create k6 script

---
+++ k6/script.js
@@ -0,0 +1,17 @@
+import http from 'k6/http';
+import { check } from 'k6';
+import { Rate } from 'k6/metrics';
+
+export const errorRate = new Rate('errors');
+
+export default function () {
+    const url = 'http://127.0.0.1:61949/swagger-ui/index.html';
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    check(http.get(url, params), {
+        'status is 200': (r) => r.status === 200,
+    }) || errorRate.add(1);
+}
