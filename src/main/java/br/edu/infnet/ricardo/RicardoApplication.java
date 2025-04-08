a625c1a38e3a305d6fe06b0e87248647295eaf9d
Ricardo da Silva Lima <rslima@gmail.com>
2025-04-07 21:51:40 -0300

    OpenAPI doc

--- src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
+++ src/main/java/br/edu/infnet/ricardo/RicardoApplication.java
@@ -1,10 +1,16 @@
 package br.edu.infnet.ricardo;
 
 import io.micrometer.core.instrument.MeterRegistry;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.utils.SpringDocAnnotationsUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class RicardoApplication {
@@ -16,6 +22,20 @@ public class RicardoApplication {
     @Bean
     MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
         return registry -> registry.config().commonTags("application", "Ricardo");
+    }
+
+    @Bean
+    public OpenAPI openAPI() {
+        SpringDocAnnotationsUtils.removeAnnotationsToIgnore(Map.class);
+        return new OpenAPI()
+                .info(
+                        new Info().title("API do Ricardo")
+                                .version("0.0.5")
+                                .description("API para acesso às funcionalidades do controle de dieta")
+                                .contact(
+                                        new Contact().name("Ricardo da Silva Lima")
+                                                .email("rslima@gmail.com")))
+                ;
     }
 
 }
